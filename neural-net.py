7b5c2feb4758bd6277279b0d5487807bf1f15913
Arshin <arshin@Arshins-MBP.hsd1.ca.comcast.net>
2018-09-03 02:21:13 -0700

    refactor var name

--- neural-net.py
+++ neural-net.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy
 from numpy import exp, array, random, dot
 
 def nonlin(x, deriv=False):
@@ -7,33 +7,33 @@ def nonlin(x, deriv=False):
     return 1/(1+exp(-x))
 
 # Input data
-X = np.array([[0,0,1],
+X = numpy.array([[0,0,1],
                 [0,1,1],
                 [1,0,1],
                 [1,1,1]])
 
 # Output data
-y = np.array([[0],
+y = numpy.array([[0],
             [1],
             [1],
             [0]])
 
-np.random.seed(1)
+numpy.random.seed(1)
 
 # Synapses
-syn0 = 2 * np.random.random((3,4)) - 1
-syn1 = 2 * np.random.random((4,1)) - 1
+syn0 = 2 * numpy.random.random((3,4)) - 1
+syn1 = 2 * numpy.random.random((4,1)) - 1
 
 # Training Step
 for j in xrange(60000):
     l0 = X
-    l1 = nonlin(np.dot(l0, syn0))
-    l2 = nonlin(np.dot(l1, syn1))
+    l1 = nonlin(numpy.dot(l0, syn0))
+    l2 = nonlin(numpy.dot(l1, syn1))
 
     l2_error = y - l2
 
     if (j%10000) == 0:
-        print "Error: " + str(np.mean(np.abs(l2_error)))
+        print "Error: " + str(numpy.mean(numpy.abs(l2_error)))
 
     l2_delta = l2_error * nonlin(l2, deriv=True)
 
